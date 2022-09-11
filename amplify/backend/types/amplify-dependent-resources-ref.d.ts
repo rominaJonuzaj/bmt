45e6e7ed4e0a4e63f39dd1380fcf420b25fb4a38
Romina <romina.jonuzaj@inspire11.com>
2022-09-11 20:29:54 +0200

    amplify connected

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "bmtapp3dd253a6": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
