39560ae18868974bcc0e2b0f98226913eab3fc4e
Romina <romina.jonuzaj@inspire11.com>
2022-10-12 04:16:06 +0200

    added settings funcion

--- src/screens/settingsScreen/SettingsScreen.tsx
+++ src/screens/settingsScreen/SettingsScreen.tsx
@@ -3,90 +3,50 @@ import { useContext, useState } from "react";
 import { View } from "react-native";
 import { Text } from "../../components/StyledText";
 import SwitchButton from "../../components/buttons/switchButton";
-// import { SecureStorageContext } from "../../contexts/SecureStorageContext";
-// import { UserContext } from "../../contexts/UserContext";
 import styles from "./SettingsScreen.styles";
 import Container from "../../components/container/Container";
 
 const SettingsScreen = () => {
-//   const { dispatch } = useContext(SecureStorageContext);
-//   const user = useContext(UserContext);
-
-//   const [switchButtons, setSwitchButtons] = useState({
-//     isTemperaturePreferenceButtonEnabled: user.weatherUnitPreference === "celsius" ? true : false,
-//     isTimeFormatButtonEnabled: user.timeFormatPreference === "military" ? true : false,
-//   });
-
-//   const handleTemperatureSwitch = async () => {
-//     try {
-//       setSwitchButtons({
-//         ...switchButtons,
-//         isTemperaturePreferenceButtonEnabled: !switchButtons.isTemperaturePreferenceButtonEnabled,
-//       });
-
-//       !switchButtons.isTemperaturePreferenceButtonEnabled
-//         ? dispatch({ type: "SET_WEATHER_UNIT_PREFERENCE", value: "celsius" })
-//         : dispatch({ type: "SET_WEATHER_UNIT_PREFERENCE", value: "fahrenheit" });
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-
-//   const handleTimeFormatSwitch = async () => {
-//     try {
-//       !switchButtons.isTimeFormatButtonEnabled
-//         ? dispatch({ type: "SET_TIME_FORMAT_PREFERENCE", value: "military" })
-//         : dispatch({ type: "SET_TIME_FORMAT_PREFERENCE", value: "standard" });
-
-//       setSwitchButtons({
-//         ...switchButtons,
-//         isTimeFormatButtonEnabled: !switchButtons.isTimeFormatButtonEnabled,
-//       });
-//     } catch (e) {
-//       console.log(e);
-//     }
-//   };
-
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const [isEnabledInt, setIsEnabledInt] = useState(false);
+  const toggleSwitchInt = () => setIsEnabledInt(previousState => !previousState);
+  const [isEnabledPos, setIsEnabledPos] = useState(false);
+  const toggleSwitchPos = () => setIsEnabledPos(previousState => !previousState);
+  const [isEnabledLove, setIsEnabledLove] = useState(false);
+  const toggleSwitchLove = () => setIsEnabledLove(previousState => !previousState);
+  const [isEnabledMind, setIsEnabledMind] = useState(false);
+  const toggleSwitchMind = () => setIsEnabledMind(previousState => !previousState);
   return (
     <Container>
       <View style={styles.container}>
         <View style={styles.switchContainer}>
           <Text style={styles.TextTitle}>Cfarë dëshironi të punoni sot?</Text>
           <SwitchButton
-            onClick={null}
-            isEnabled={null}
-            label="Turn Off Negative Thoughts"
+            onClick={toggleSwitch}
+            isEnabled={isEnabled}
+            label="Turn Off Mendimet Negative"
           />
            <SwitchButton
-            onClick={null}
-            isEnabled={null}
-            label="Turn Off Inttusive Thoughts"
+            onClick={toggleSwitchInt}
+            isEnabled={isEnabledInt}
+            label="Turn Off Mendimet Intrusive"
           />
            <SwitchButton
-            onClick={null}
-            isEnabled={null}
-            label="Turn On Positive Thoughts"
+            onClick={toggleSwitchPos}
+            isEnabled={isEnabledPos}
+            label="Turn On Mendimet Positive"
           />
            <SwitchButton
-            onClick={null}
-            isEnabled={null}
+            onClick={toggleSwitchLove}
+            isEnabled={isEnabledLove}
             label="Turn On: Love For Myself"
           />
            <SwitchButton
-            onClick={null}
-            isEnabled={null}
+            onClick={toggleSwitchMind}
+            isEnabled={isEnabledMind}
             label="Turn On Good Mindset"
           />
-           <SwitchButton
-            onClick={null}
-            isEnabled={null}
-            label="Turn Off Impulsive Triggers"
-          />
-          {/* <SwitchButton
-            onClick={handleTimeFormatSwitch}
-            isEnabled={switchButtons.isTimeFormatButtonEnabled}
-            label="Switch to Military Time Format"
-          /> */}
         </View>
       </View>
     </Container>
